c2940bc22488f47c1d0df9cd467a5be3b27a4dc6
Jacob Hrbek <kreyren@rixotstudio.cz>
2020-09-05 17:15:40 +0200

    Forced to indent the region according to elisp standard
    Signed-off-by: Jacob Hrbek <kreyren@rixotstudio.cz>

--- src/RXT0128/codeblocks/find-emacs-dirs.el
+++ src/RXT0128/codeblocks/find-emacs-dirs.el
@@ -17,36 +17,30 @@
 ;;;!
 ;;;! WARNING: Use only standard functions as this is used at the header of the file during initialization to find the directory
 (cond
-	((> (length (getenv "EMACS_DIR")) 0)
-		(setq user-emacs-directory (getenv "EMACS_DIR"))
-	)
-	;; If EMACS_DIR variable is unset or blank
-	((= (length (getenv "EMACS_DIR")) 0)
-		(cl-case system-type
-			((gnu gnu/linux)
-				(cond
-					((> (length (getenv "XDG_CONFIG_HOME")) 0)
-						;; FIXME-QA: Implement check for expected directory
-						(setq user-emacs-directory (concat (getenv "XDG_CONFIG_HOME") "/" emacs-distro-name)) )
-					((= (length (getenv "XDG_CONFIG_HOME")) 0)
-						;; FIXME-QA: Implement check for expected directory
-						(setq user-emacs-directory (concat (getenv "HOME") "/" emacs-distro-name)) )
-					(t
-						(message "Unexpected happend while trying to find custom emacs distribution directory which is most likely a bug, you might want to use environment variable 'EMACS_DIR'")
-						(kill-emacs 250) )
-				)
-			)
-			((windows-nt ms-dos)
-				(princ "FIXME: Platform '%1$s' is not implemented in find-emacs-dirs.el codeblock")
-				(kill-emacs 28)	)
-			((gnu/kfreebsd darwin cygwin aix berkeley-unix hpux irix usg-unix-v)
-				(princ "FIXME: Platform '%1$s' is unimplemented aldo theoretically supported, exitting for safety as it was not tested" system-type)
-				(kill-emacs 28)	)
-			(t
-				(princ "Unexpected happend while processing system-type variable storing '%1$s', guessing not implemented?" system-type) )
-		)
-	)
-	(t
-		(message "Unexpected happend while looking for emacs directories in codeblock 'find-emacs-dirs.el'")
-		(kill-emacs 255) )
-)
+ ((> (length (getenv "EMACS_DIR")) 0)
+  (setq user-emacs-directory (getenv "EMACS_DIR")))
+ ;; If EMACS_DIR variable is unset or blank
+ ((= (length (getenv "EMACS_DIR")) 0)
+  (cl-case system-type
+    ((gnu gnu/linux)
+     (cond
+      ((> (length (getenv "XDG_CONFIG_HOME")) 0)
+       ;; FIXME-QA: Implement check for expected directory
+       (setq user-emacs-directory (concat (getenv "XDG_CONFIG_HOME") "/" emacs-distro-name)))
+      ((= (length (getenv "XDG_CONFIG_HOME")) 0)
+       ;; FIXME-QA: Implement check for expected directory
+       (setq user-emacs-directory (concat (getenv "HOME") "/" emacs-distro-name)))
+      (t
+       (message "Unexpected happend while trying to find custom emacs distribution directory which is most likely a bug, you might want to use environment variable 'EMACS_DIR'")
+       (kill-emacs 250))))
+    ((windows-nt ms-dos)
+     (princ "FIXME: Platform '%1$s' is not implemented in find-emacs-dirs.el codeblock")
+     (kill-emacs 28))
+    ((gnu/kfreebsd darwin cygwin aix berkeley-unix hpux irix usg-unix-v)
+     (princ "FIXME: Platform '%1$s' is unimplemented aldo theoretically supported, exitting for safety as it was not tested" system-type)
+     (kill-emacs 28))
+    (t
+     (princ "Unexpected happend while processing system-type variable storing '%1$s', guessing not implemented?" system-type))))
+ (t
+  (message "Unexpected happend while looking for emacs directories in codeblock 'find-emacs-dirs.el'")
+  (kill-emacs 255)))
